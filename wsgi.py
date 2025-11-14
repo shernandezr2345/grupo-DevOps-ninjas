ea8dd16f956c004c36b6b2d5bdc8c173ee869665
wareval0 <w.arevalo@uniandes.edu.co>
2025-11-13 22:28:32 -0500

    AWS Fargate

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@ from app import create_app
 
 application = create_app() 
 
-if __name__ == "__main__":
-    application.run(host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     application.run(host="0.0.0.0", port=8000)
